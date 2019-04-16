51dedff28f2d9fa7a25980cf98c416394b1dde14
Anna Beagley <annabeagley@Annas-MacBook-Air.local>
2019-04-16 11:29:33 -0600

    Added lightswitch images to buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import "./index.css"
+import "./index.css";
+import lightswitchon from './light-switch-on.png'
+import Lightswitchoff from './light-switch-off.png'
 
 
 
@@ -11,8 +13,10 @@ const Hooks = () => {
     return (
         <div className={`Hooks ${brightness}`}>
             <br />
-            <button onClick={() => setOn(true)}>On</button>
-            <button onClick={() => setOn(false)}>Off</button>
+            <button onClick={() => setOn(true)}>
+            <img src={lightswitchon} width="75" height="75" alt="LightSwitch"/>
+</button>
+            <button onClick={() => setOn(false)}><img src={Lightswitchoff} background="black" width="75" height="75" alt="LightSwitch"/></button>
             <br />
         </div>
     )
